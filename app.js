74a0d5f44fdb89d794188ce285242874dd1e0005
Aython Houttekier <Aython Houttekier>
2018-11-08 13:06:57 +0100

    Adding a createPNGStream

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const { createCanvas } = require('canvas')
 const canvas = createCanvas(96, 64)
 const ctx = canvas.getContext('2d')
 
-ctx.font = '35px Impact'
-ctx.fillText('15°C', 30, 30)
+ctx.font = '45px Impact'
+ctx.fillText('15°C', 10, 50)
 console.log('<img src="' + canvas.toDataURL(canvas, '#ffffff') + '" />')
 
-const out = fs.createWriteStream(__dirname + '/temperature.png')
+const out = fs.createWriteStream('images/temperature.png')
 const stream = canvas.createPNGStream()
 stream.pipe(out)
 out.on('finish', () =>  console.log('The PNG file was created.'))
