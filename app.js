8f187a03e73a17a1114c83f6fffa6b687a3acc97
Aython Houttekier <Aython Houttekier>
2018-11-24 15:23:53 +0100

    implement ttn data in png

--- app.js
+++ app.js
@@ -4,12 +4,41 @@ const fs = require('fs')
 const { createCanvas } = require('canvas')
 const canvas = createCanvas(96, 64)
 const ctx = canvas.getContext('2d')
+//Express
+const express = require('express')
+const app = express()
+const port = 3000
 
-ctx.font = '45px Impact'
-ctx.fillText('15°C', 10, 50)
-console.log('<img src="' + canvas.toDataURL(canvas, '#ffffff') + '" />')
+var ttn = require("ttn")
+var appID = "iot-tv"
+var accessKey = "ttn-account-v2.Mf7Yb3RcFvB2q7wV09I1nbihROIlSrN_N9dSPc1xEWs"
 
-const out = fs.createWriteStream('images/temperature.png')
-const stream = canvas.createPNGStream()
-stream.pipe(out)
-out.on('finish', () =>  console.log('The PNG file was created.'))
+ttn.data(appID, accessKey)
+.then(function (client) {
+    client.on("uplink", function (devID, payload) {
+        var devID = "sensor-02"
+        console.log("Received uplink from ", devID)
+        console.log(payload.payload_fields.temperature)
+
+        ctx.font = '20px Impact'
+        ctx.fillStyle = "#ff0000";
+        ctx.fillText(payload.payload_fields.temperature + "°C", 10, 50)
+
+        //image source in de console
+        console.log('<img src="' + canvas.toDataURL(canvas, '#ffffff') + '" />')
+        
+        //opslaan van afbeeldiing in images map
+        const out = fs.createWriteStream('images/temperature.png')
+        const stream = canvas.createPNGStream()
+        stream.pipe(out)
+        out.on('finish', () =>  console.log('The PNG file was created.'))
+    })
+})
+.catch(function (error) {
+    console.error("Error", error)
+    process.exit(1)
+})
+
+//Express
+app.get('/', (req, res) => res.send(canvas.toDataURL(canvas)))
+app.listen(port, () => console.log(`Temperature app listening on port ${port}!`))
