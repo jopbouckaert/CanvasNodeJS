03dac11d9e52fa1b952a65dd05b5ac89bbd22399
Aython Houttekier <Aython Houttekier>
2018-12-07 10:43:52 +0100

    listen to post and putting data in image and sending base64code as response

--- app.js
+++ app.js
@@ -1,51 +1,46 @@
 //POST 172.16.1.12/api/image body: image: base 64(bytes)->png format 96x64px
-
 const fs = require('fs')
 const { createCanvas } = require('canvas')
 const canvas = createCanvas(96, 64)
 const ctx = canvas.getContext('2d')
+
+//bodyparser
+const bodyParser = require('body-parser');
+
 //Express
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
-var ttn = require("ttn")
-var appID = "iot-tv"
-var accessKey = "ttn-account-v2.Mf7Yb3RcFvB2q7wV09I1nbihROIlSrN_N9dSPc1xEWs"
-
-ttn.data(appID, accessKey)
-.then(function (client) {
-    client.on("uplink", function (devID, payload) {
-        var devID = "sensor-02"
-        console.log("Received uplink from ", devID)
-        console.log(payload)
-        console.log(payload.payload_fields.counter)
-       
-        height = canvas.height
-        width = canvas.width
-        ctx.clearRect(0,0,width,height);    
-        ctx.font = '20px Impact'
-        ctx.fillStyle = "#ff0000";
-        ctx.fillText(payload.payload_fields.temperature + "°C", 10, 50)
-        //image source in de console
-        //console.log('<img src="' + canvas.toDataURL(canvas, '#ffffff') + '" />')
-        
-        //opslaan van afbeeldiing in images map
-        const out = fs.createWriteStream('images/temperature.png')
-        const stream = canvas.createPNGStream()
-        stream.pipe(out)
-        out.on('finish', () =>  console.log('The PNG file was created.'))
-
-    })
-})
-.catch(function (error) {
-    console.error("Error", error)
-    process.exit(1)
+var jsonParser = bodyParser.json() 
+  
+  app.listen(port, '0.0.0.0', function (err) {
+    if (err) {
+      throw err
+    }
+    
+    //Express
+    console.log(`Temperature app listening on port ${port}!`)
 })
 
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-
-//Express
-//app.get('/', (req, res) => res.send(canvas.toDataURL(canvas)))
-//app.listen(port, () => console.log(`Temperature app listening on port ${port}!`))
+app.post('/', jsonParser, (req, res) => {
+  console.log('request on /')
+  var data = req.body.value;
+  console.log(req.body);
+  console.log(req.body.value);
+  //image
+  height = canvas.height
+  width = canvas.width
+    ctx.clearRect(0,0,width,height)    
+    ctx.font = '28px Impact'
+    ctx.fillStyle = "#ff0000"
+    ctx.fillText(data + "°C", 5, 40)
+    //image source in de console
+    //console.log('<img src="' + canvas.toDataURL(canvas, '#ffffff') + '" />')
+    //opslaan van afbeeldiing in images map
+    const out = fs.createWriteStream('images/temperature.png')
+    const stream = canvas.createPNGStream()
+    stream.pipe(out)
+    out.on('finish', () =>  console.log('The PNG file was created.'))
+    res.send(canvas.toDataURL(canvas))
+  })
