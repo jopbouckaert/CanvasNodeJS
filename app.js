76bb4ecd46873ffc929b081466515d2e91c4ff41
Aython Houttekier <Aython Houttekier>
2018-12-09 12:55:18 +0100

    make code more readable and removing some comments

--- app.js
+++ app.js
@@ -1,46 +1,44 @@
-//POST 172.16.1.12/api/image body: image: base 64(bytes)->png format 96x64px
+//canvas
 const fs = require('fs')
 const { createCanvas } = require('canvas')
 const canvas = createCanvas(96, 64)
 const ctx = canvas.getContext('2d')
 
+//imagestream
+const out = fs.createWriteStream('images/temperature.png')
+const stream = canvas.createPNGStream()
+
 //bodyparser
 const bodyParser = require('body-parser');
+var jsonParser = bodyParser.json() 
 
 //Express
 const express = require('express')
 const app = express()
 const port = 4000
 
-var jsonParser = bodyParser.json() 
-  
-  app.listen(port, '0.0.0.0', function (err) {
+app.listen(port, '0.0.0.0', function (err) {
     if (err) {
       throw err
     }
-    
-    //Express
-    console.log(`Temperature app listening on port ${port}!`)
+      console.log(`Temperature app listening on port ${port}!`)
 })
 
 app.post('/', jsonParser, (req, res) => {
-  console.log('request on /')
-  var data = req.body.value;
-  console.log(req.body);
-  console.log(req.body.value);
-  //image
-  height = canvas.height
-  width = canvas.width
+    var data = req.body.value;
+    console.log(req.body);
+    console.log("------------------------------------")
+    console.log(req.body.value);
+  
+    //image
+    height = canvas.height
+    width = canvas.width
     ctx.clearRect(0,0,width,height)    
-    ctx.font = '28px Impact'
+    ctx.font = '25px Impact'
     ctx.fillStyle = "#ff0000"
     ctx.fillText(data + "°C", 5, 40)
-    //image source in de console
-    //console.log('<img src="' + canvas.toDataURL(canvas, '#ffffff') + '" />')
-    //opslaan van afbeeldiing in images map
-    const out = fs.createWriteStream('images/temperature.png')
-    const stream = canvas.createPNGStream()
+
     stream.pipe(out)
     out.on('finish', () =>  console.log('The PNG file was created.'))
     res.send(canvas.toDataURL(canvas))
-  })
+})
