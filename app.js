01e65039af0ab9aac870979e1b1d2c1258b7b501
Jop Bouckaert <jop.bouckaert@outlook.com>
2019-01-30 14:16:22 +0100

    update app.js with Jimp

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-//canvas
-const fs = require('fs')
-const { createCanvas } = require('canvas')
-const canvas = createCanvas(96, 64)
-const ctx = canvas.getContext('2d')
+// //canvas
+// const fs = require('fs')
+// const { createCanvas } = require('canvas')
+// const canvas = createCanvas(96, 64)
+// const ctx = canvas.getContext('2d')
 
-//imagestream
-const out = fs.createWriteStream('images/temperature.png')
-const stream = canvas.createPNGStream()
+var Jimp = require('jimp');
+
 
 //bodyparser
 const bodyParser = require('body-parser');
@@ -24,21 +23,43 @@ app.listen(port, '0.0.0.0', function (err) {
       console.log(`Temperature app listening on port ${port}!`)
 })
 
-app.post('/', jsonParser, (req, res) => {
-    var data = req.body.value;
-    console.log(req.body);
-    console.log("------------------------------------")
-    console.log(req.body.value);
-  
-    //image
-    height = canvas.height
-    width = canvas.width
-    ctx.clearRect(0,0,width,height)    
-    ctx.font = '25px Impact'
-    ctx.fillStyle = "#ff0000"
-    ctx.fillText(data + "°C", 5, 40)
 
-    stream.pipe(out)
-    out.on('finish', () =>  console.log('The PNG file was created.'))
-    res.send(canvas.toDataURL(canvas))
+
+app.post('/', jsonParser, (req, res) => {
+    var totalpeople = req.body.total;
+    console.log(req.total);
+    console.log("------------------------------------")
+    console.log(req.body.total);
+    var currentpeople = req.body.current;
+    console.log(req.current);
+    console.log("------------------------------------")
+    console.log(req.body.current);
+
+    new Jimp(96, 64, 0x000000ff, (err, image) => {
+      image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
+          var red = this.bitmap.data[idx + 0];
+          var green = this.bitmap.data[idx + 1];
+          var blue =  this.bitmap.data[idx + 2];
+          var alpha = this.bitmap.data[idx + 3];
+    
+          if(x == image.bitmap.width - 1 || y == image.bitmap.height - 1 || x ==0 || y == 0){
+            this.bitmap.data[idx + 0] = 0xFF;
+           }
+      });
+
+      Jimp.loadFont(Jimp.FONT_SANS_16_WHITE).then(font => {
+        image.print(font, 10, 10, 'Current: ' + totalpeople);
+        image.print(font, 10, 30, 'Total: ' + currentpeople);
+  
+        image.getBase64Async("image/png").then(result => {
+          res.send(result);
+        }).catch(err => {
+          console.log("Failed to get the base 64 representation");
+        })
+      })
+      .catch(err => {
+        console.log("Failed to load font");
+      });
+  
+  });
 })
